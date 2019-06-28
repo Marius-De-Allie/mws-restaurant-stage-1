1e46643c09f38659a348d0d210940977c4122050
Marius De Allie <spectre4300s@gmail.com>
2019-06-28 16:26:57 -0400

    Remove variable more's href property from '/js/main/js' file
    Remove variable more's href property
    'more.href = DBHelper.urlForRestaurant(restaurant)' from
    '/js/main/js' file

--- js/main.js
+++ js/main.js
@@ -187,7 +187,6 @@ createRestaurantHTML = (restaurant) => {
   // Create new button element for more details on restaurant.
   const more = document.createElement('button');
   more.innerHTML = 'View Details';
-  more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
 
   return li
