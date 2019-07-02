cbaad6a000f443c968130a3ca424db43ed46f8c2
Marius De Allie <spectre4300s@gmail.com>
2019-07-02 19:15:27 -0400

    Update file '/js/sw.js' to abide by Udacity's JavaScript style guidelines

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
-const cacheFiles = ['/', '/index.html', '/restaurant.html', '/css/styles.css',
-'/js/dbhelper.js', '/js/main.js', '/js/restaurant_info.js',
-'/data/restaurants.json', '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg'
-, '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg',
-'/img/10.jpg'
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
 ];
 // Event listener that listens for install event completion on the service worker.
 self.addEventListener('install', function(e) {
