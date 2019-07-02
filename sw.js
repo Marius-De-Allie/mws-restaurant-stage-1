6c78596dd255a79b83138a029235d2b854cb6077
Marius De Allie <spectre4300s@gmail.com>
2019-07-02 17:48:23 -0400

    Update file '/js/sw.js' to abide by Udacity's JavaScript style guidelines

--- sw.js
+++ sw.js
@@ -1,8 +1,22 @@
-const cacheFiles = ['/', '/index.html', '/restaurant.html', '/css/styles.css',
-'/js/dbhelper.js', '/js/main.js', '/js/restaurant_info.js',
-'/data/restaurants.json', '/img/1.jpg', '/img/2.jpg', '/img/3.jpg', '/img/4.jpg'
-, '/img/5.jpg', '/img/6.jpg', '/img/7.jpg', '/img/8.jpg', '/img/9.jpg',
-'/img/10.jpg'
+const cacheFiles = [
+  '/',
+  '/index.html',
+  '/restaurant.html',
+  '/css/styles.css',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg'
 ];
 // Event listener that listens for install event completion on the service worker.
 self.addEventListener('install', function(e) {
@@ -23,7 +37,7 @@ self.addEventListener('fetch', function(e) {
           return fetch(e.request).then(function(response) {
             const responseClone = response.clone();
             caches.open('cache1').then(function(cache) {
-              cache.put(e.request, responseClone);
+              cache.put(e.request, responseClone); // if requested resource not yet in cache1, add to cache1.
             })
             return response;
           })
